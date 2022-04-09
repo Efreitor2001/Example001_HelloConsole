c8e481c6b915630f7a0f323cb1e1165e954e3660
Efreitor2001 <pokimoha.123@mail.com>
2022-04-09 18:30:12 +0300

    add Example 003 Sum

---
+++ Example003_Sum/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите первое число...");
+var numA = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число...");
+var numB = int.Parse(Console.ReadLine());
+int result = numA + numB;
+Console.Write(numA);
+Console.Write(" + ");
+Console.Write(numB);
+Console.Write(" = ");
+Console.Write(result);
